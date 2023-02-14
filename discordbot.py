712f5b9cc0361c5dd7b62f3b3a07f4e79dbde635
doldolmarine <109653305+doldolmarine@users.noreply.github.com>
2023-02-14 20:45:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,19 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
 import discord
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-PREFIX = os.environ['PREFIX']
-TOKEN = os.environ['TOKEN']
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}.')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+from discord.ext import commands
+while True:
+    comdo =';'
+    app = commands.Bot(intents
+                       =discord.Intents.default(), command_prefix=comdo ) #command_prefix=comdo     
+    @app.event
+    async def on_ready():
+        print('다음으로 로그인합니다: {}'.format(app.user.name))
+        await app.change_presence(status=discord.Status.do_not_disturb  , activity=None, )
 
 
-try:
-    client.run(TOKEN)
-except discord.errors.LoginFailure as e:
-    print("Improper token has been passed.")
+    @app.event
+    async def sayon(ctx):
+        if ctx.content.startswith('ab'):
+            await ctx.channel.sand('반갑습니다')
+        
+
+    app.run('MTA3NDkxNDg5ODMwNTM1NTc3Ng.GF672Y.BEprEDtepCdDE-p6_jMeXQKAQKYAv88pUXXu5Y')
